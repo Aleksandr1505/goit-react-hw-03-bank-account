0cdb2b466dc5e61b3ebda325f387db768ec45ec7
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-09-09 15:16:31 +0300

    Updates

---
+++ precache-manifest.017657cc2afbddfc5f7242273907ba55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e759fcf1f68124aa6ea236eec0c075bd",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "125c43d753b07ed6dd44",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.cc8d4260.chunk.css"
+  },
+  {
+    "revision": "eed2e264e78cef1cc6f0",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.9641c7a1.chunk.js"
+  },
+  {
+    "revision": "125c43d753b07ed6dd44",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.de608547.chunk.js"
+  },
+  {
+    "revision": "3abcae2be7dde6a72721",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.a2b09cc2.js"
+  }
+]);
